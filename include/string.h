8d5654209534a3c9dc31834a97835e8ad4a7bf2f
Yunde Liu <realyunde@outlook.com>
2020-08-01 16:15:07 +0800

    refactor(string.h): delete 'inline'

--- include/string.h
+++ include/string.h
@@ -24,44 +24,44 @@ extern char * strerror(int errno);
  *		(C) 1991 Linus Torvalds
  */
 
-extern inline char * strcpy(char * dest,const char *src);
+extern char * strcpy(char * dest,const char *src);
 
-extern inline char * strncpy(char * dest,const char *src,int count);
+extern char * strncpy(char * dest,const char *src,int count);
 
-extern inline char * strcat(char * dest,const char * src);
+extern char * strcat(char * dest,const char * src);
 
-extern inline char * strncat(char * dest,const char * src,int count);
+extern char * strncat(char * dest,const char * src,int count);
 
-extern inline int strcmp(const char * cs,const char * ct);
+extern int strcmp(const char * cs,const char * ct);
 
-extern inline int strncmp(const char * cs,const char * ct,int count);
+extern int strncmp(const char * cs,const char * ct,int count);
 
-extern inline char * strchr(const char * s,char c);
+extern char * strchr(const char * s,char c);
 
-extern inline char * strrchr(const char * s,char c);
+extern char * strrchr(const char * s,char c);
 
-extern inline int strspn(const char * cs, const char * ct);
+extern int strspn(const char * cs, const char * ct);
 
-extern inline int strcspn(const char * cs, const char * ct);
+extern int strcspn(const char * cs, const char * ct);
 
-extern inline char * strpbrk(const char * cs,const char * ct);
+extern char * strpbrk(const char * cs,const char * ct);
 
-extern inline char * strstr(const char * cs,const char * ct);
+extern char * strstr(const char * cs,const char * ct);
 
-extern inline int strlen(const char * s);
+extern int strlen(const char * s);
 
 extern char * ___strtok;
 
-extern inline char * strtok(char * s,const char * ct);
+extern char * strtok(char * s,const char * ct);
 
-extern inline void * memcpy(void * dest,const void * src, int n);
+extern void * memcpy(void * dest,const void * src, int n);
 
-extern inline void * memmove(void * dest,const void * src, int n);
+extern void * memmove(void * dest,const void * src, int n);
 
-extern inline int memcmp(const void * cs,const void * ct,int count);
+extern int memcmp(const void * cs,const void * ct,int count);
 
-extern inline void * memchr(const void * cs,char c,int count);
+extern void * memchr(const void * cs,char c,int count);
 
-extern inline void * memset(void * s,char c,int count);
+extern void * memset(void * s,char c,int count);
 
 #endif
