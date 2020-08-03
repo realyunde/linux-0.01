3516259bec2ba6d0a628329da8f180e235c63999
Yunde Liu <realyunde@outlook.com>
2020-08-03 15:03:50 +0800

    refactor(boot/boot.asm): delete 'jmp $+2' ins
    The jmps in between are for synchronizing CPU and PIC, so the CPU won't send data the PIC cannot receive yet.
    This is comparable to wait states when writing to memory: when the CPU is faster than the memory/memory arbiter, it needs to wait some time before accessing memory the next time.
    Maybe can read data from some special IO port to judgment PIC status, fix later.
    References:
    1. https://stackoverflow.com/questions/34561275/setting-up-interrupts-in-protected-mode-x86

--- boot/boot.asm
+++ boot/boot.asm
@@ -118,29 +118,20 @@ end_move:
 
         mov     al,0x11                ; initialization sequence
         out     0x20,al                ; send it to 8259A-1
-        dw      0x00eb,0x00eb          ; jmp $+2, jmp $+2
         out     0xA0,al                ; and to 8259A-2
-        dw      0x00eb,0x00eb
         mov     al,0x20                ; start of hardware int's (0x20)
         out     0x21,al
-        dw      0x00eb,0x00eb
         mov     al,0x28                ; start of hardware int's 2 (0x28)
         out     0xA1,al
-        dw      0x00eb,0x00eb
         mov     al,0x04                ; 8259-1 is master
         out     0x21,al
-        dw      0x00eb,0x00eb
         mov     al,0x02                ; 8259-2 is slave
         out     0xA1,al
-        dw      0x00eb,0x00eb
         mov     al,0x01                ; 8086 mode for both
         out     0x21,al
-        dw      0x00eb,0x00eb
         out     0xA1,al
-        dw      0x00eb,0x00eb
         mov     al,0xFF                ; mask off all interrupts for now
         out     0x21,al
-        dw      0x00eb,0x00eb
         out     0xA1,al
 
         ; switch from real to protected mode
@@ -151,7 +142,6 @@ end_move:
 
 
 empty_8042:
-        dw      0x00eb,0x00eb
         in      al,0x64         ; 8042 status port
         test    al,2            ; is input buffer full?
         jnz     empty_8042      ; yes - loop
